f9917390ca0bef802b1849557ab224769920e434
Van Tien Nguyen <tiennv@L-COM-4.utsarr.net>
2023-11-17 02:56:36 -0600

    add training random forest

--- Project/data/data.py
+++ Project/data/data.py
@@ -1,10 +1,15 @@
-import json
+import json, os
+import pandas as pd
 
 
 class Dataset:
-    def __init__(self, data_file):
+    def __init__(self, data_file: str, label_map_path: str, label_map_invert_path: str):
         self._data_path = data_file
+        self._label_map_path = label_map_path
+        self._label_map_invert_path = label_map_invert_path
         self._raw_data = None
+        self._label_map = None
+        self._label_map_invert = None
         self._data = None
         self._X = None
         self._y = None
@@ -12,7 +17,11 @@ class Dataset:
     def load_data_from_json(self):
         with open(self._data_path, 'r') as f:
             self._raw_data = json.load(f)
-        
+        with open(self._label_map_path, 'r', encoding='utf-8') as f:
+            self._label_map = json.load(f)
+        with open(self._label_map_invert_path, 'r', encoding='utf-8') as f:
+            self._label_map_invert = json.load(f)
+
     def _char_label_to_int(self, char):
         return ord(char)
     
@@ -24,27 +33,57 @@ class Dataset:
         for passwd_str, encodings in self._raw_data.items():
             if len(passwd_str) < 7:
                 continue
-            # print("Entire password: ", passwd_str)
-            # for i in range(len(passwd_str)-6):
-            #     print("\tInput 6-order prefix: ", passwd_str[i:i+6])
-            #     print("\tEncoding: ", encodings[i])
-            #     print("\tLabel: ", passwd_str[i+6])
-            #     print("\tEncoding label: ", ord(passwd_str[i+6]))
-            #     input()
-            for i in range(len(passwd_str)-6):
+            k = 0
+            while k < len(passwd_str)-6:
                 self._data.append(
                     {
                         'entire_pass': passwd_str,
-                        'input_prefix': passwd_str[i: i+6],
+                        'start_index_prefix': k,
+                        'input_prefix': passwd_str[k: k+6],
+                        'input_encoding': encodings[k],
+                        'flatten_input_encoding': [x for sub_encode in encodings[k] for x in sub_encode],
+                        'next_char': passwd_str[k+6],
+                        'label': self._label_map[passwd_str[k+6]]
                     }
                 )
-
-            
+                k += 1
+            # At the end, add the end character.
+            self._data.append(
+                {
+                    'entire_pass': passwd_str,
+                    'start_index_prefix': k,
+                    'input_prefix': passwd_str[k: k+6],
+                    'input_encoding': encodings[k],
+                    'flatten_input_encoding': [x for sub_encode in encodings[k] for x in sub_encode],
+                    'next_char': 'Es',
+                    'label': self._label_map['Es']
+                }
+            )
+        self._X = [x['flatten_input_encoding'] for x in self._data]
+        self._y = [x['label'] for x in self._data]
         
+    def save_to_csv(self, data_file, entire_data=False):
+        if entire_data:
+            df = pd.DataFrame(self._data)
+        else:
+            df = {i:[] for i in range(26)}
+            df['label'] = []
+            for x, label in zip(self._X, self._y):
+                for i in range(len(x)):
+                    df[i].append(x[i])
+                df['label'].append(label)
+            df = pd.DataFrame(df)
+        df.to_csv(data_file, index=False)
 
 
 if __name__ == '__main__':
     data_path = '/home/tiennv/Github/EE6363_AdvancedML/Project/output/encoded_password.json'
-    dataloader = Dataset(data_file=data_path)
+    label_mapping_path = '/home/tiennv/Github/EE6363_AdvancedML/Project/data/label_mapping.json'
+    label_mapping_invert_path = '/home/tiennv/Github/EE6363_AdvancedML/Project/data/label_mapping_invert.json'
+    output_data_folder = './data/output/'
+    entire_data_file = os.path.join(output_data_folder, 'entire.csv')
+    feature_data_file = os.path.join(output_data_folder, 'feature.csv')
+    dataloader = Dataset(data_file=data_path, label_map_path=label_mapping_path, label_map_invert_path=label_mapping_invert_path)
     dataloader.load_data_from_json()
     dataloader.process_data()
+    dataloader.save_to_csv(data_file=entire_data_file, entire_data=True)
---
+++ Project/train.py
@@ -0,0 +1,63 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import time
+from sklearn.model_selection import train_test_split
+from joblib import dump
+
+
+def load_dataset(data_file: str):
+    with open(data_file, 'r', encoding='utf-8') as f:
+        df = pd.read_csv(f)
+    X = df.iloc[:, :26].to_numpy()
+    y = df.iloc[:, -1:].to_numpy().ravel()
+    return X, y
+
+
+def create_classifer(max_depth=5, random_state=None):
+    model = RandomForestClassifier(
+        max_depth=max_depth,
+        random_state=random_state,
+    )
+    return model
+
+
+def run(data_file, max_depth, random_state, test_size):
+    # Load dataset
+    print("Loading dataset ...")
+    X, y = load_dataset(data_file=data_file)
+    print("\t--> Number of samples: {}".format(len(X)))
+
+    # Initilize clasifier
+    print("Creating classifier ...")
+    model = create_classifer(max_depth=max_depth, random_state=random_state)
+    print("\t--> Model configuratio: {}".format(model))
+
+    # Split dataset into training and testset
+    print("Spiting dataset ...")
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, shuffle=True, random_state=random_state
+    )
+    print("\t--> Training sample num: {}, testing sample num: {}".format(len(X_train), len(X_test)))
+
+    # Perform training classifer on loaded data
+    print("Performing training classifer")
+    start_time = time.time()
+    model.fit(X_train, y_train)
+    print("\t--> Training time: {} second/sample".format((time.time()-start_time)/len(X_train)))
+
+    # Perform evaluation trained model
+    print("Scoring trained model")
+    train_score = model.score(X_train, y_train)
+    test_score = model.score(X_test, y_test)
+    print("\t--> Train acc: {}, test acc: {}".format(train_score, test_score))
+
+    # Save model to disk
+    dump(model, 'model.joblib')
+    
+
+if __name__ == '__main__':
+    data_file = './data/output/feature.csv'
+    max_depth = 20
+    random_state = 1000
+    test_size =  0.2
+    run(data_file, max_depth, random_state, test_size)
